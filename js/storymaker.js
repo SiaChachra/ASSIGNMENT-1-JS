d7cfc6b86e424f5892ac45baeec0299a948f4181
Sia Chachra <siachachra@gmail.com>
2023-10-04 13:41:43 -0400

    init

--- js/storymaker.js
+++ js/storymaker.js
@@ -3,30 +3,30 @@
 /* Variables
 -------------------------------------------------- */
 // Constants for main button query selectors
-const noun1Button = document.getElementById('noun1');
-const verbButton = document.getElementById('verb');
-const adjectiveButton = document.getElementById('adjective');
-const noun2Button = document.getElementById('noun2');
-const settingButton = document.getElementById('setting');
+var noun1Button = document.getElementById('noun1');
+var verbButton = document.getElementById('verb');
+var adjectiveButton = document.getElementById('adjective');
+var noun2Button = document.getElementById('noun2');
+var settingButton = document.getElementById('setting');
 
 // Constants for p tag to display query selectors
-const choosenNoun1 = document.getElementById('choosenNoun1');
-const choosenVerb = document.getElementById('choosenVerb');
-const choosenAdjective = document.getElementById('choosenAdjective');
-const choosenNoun2 = document.getElementById('choosenNoun2');
-const choosenSetting = document.getElementById('choosenSetting');
+var choosenNoun1 = document.getElementById('choosenNoun1');
+var choosenVerb = document.getElementById('choosenVerb');
+var choosenAdjective = document.getElementById('choosenAdjective');
+var choosenNoun2 = document.getElementById('choosenNoun2');
+var choosenSetting = document.getElementById('choosenSetting');
 
 // Constants for final buttons and p tags
-const playbackButton = document.getElementById('playback');
-const randomButton = document.getElementById('random');
-const storyElement = document.getElementById('story');
+var playbackButton = document.getElementById('playback');
+var randomButton = document.getElementById('random');
+var storyElement = document.getElementById('story');
 
 // Variables for pre-defined arrays
-let nouns1 = ["cat", "dog", "elephant"];
-let verbs = ["runs", "jumps", "dances"];
-let adjectives = ["happy", "sad", "excited"];
-let nouns2 = ["ball", "tree", "computer"];
-let settings = ["park", "beach", "office"];
+let nouns1 = ["The turkey", "The dog", "mom"];
+let verbs = ["danced with", "doesn't like", "sat on"];
+let adjectives = ["scary", "sad", "a goofy"];
+let nouns2 = ["ball", "worm", "computer"];
+let settings = ["in my spaghetti", "beach", "office"];
 
 // Variables for count to grab array elements
 let noun1Count = 0;
@@ -82,6 +82,26 @@ function random_on_click() {
     playback_on_click();
 }
 
+function resetStory() {
+    // Resetting the story content
+    document.getElementById('story').textContent = '';
+
+    // Resetting the displayed choices
+    document.getElementById('choosenNoun1').textContent = '';
+    document.getElementById('choosenVerb').textContent = '';
+    document.getElementById('choosenAdjective').textContent = '';
+    document.getElementById('choosenNoun2').textContent = '';
+    document.getElementById('choosenSetting').textContent = '';
+
+    // Resetting the stored selections
+    selectedNoun1 = '';
+    selectedVerb = '';
+    selectedAdjective = '';
+    selectedNoun2 = '';
+    selectedSetting = '';
+}
+
+
 /* Event Listeners
 -------------------------------------------------- */
 document.getElementById('noun1').addEventListener('click', noun1_on_click);
@@ -91,3 +111,4 @@ document.getElementById('noun2').addEventListener('click', noun2_on_click);
 document.getElementById('setting').addEventListener('click', setting_on_click);
 document.getElementById('playback').addEventListener('click', playback_on_click);
 document.getElementById('random').addEventListener('click', random_on_click);
+document.getElementById('reset').addEventListener('click', resetStory);
